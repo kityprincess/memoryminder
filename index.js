48fd77eea8aaaa9a570e63df515e6bb0edb6ea28
kityprincess <stowekm@byui.edu>
2018-07-05 18:28:46 -0700

    install body-parser4

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
 
 const getdata = require('./models/getdata.js');
 const writedata = require('./models/writedata.js');
+const urlencodedParser = bodyParser.urlencoded({extended: false });
 
  express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -11,7 +13,7 @@ const writedata = require('./models/writedata.js');
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/vip/:idVip', getdata.getVIP)
-  .post('/newuser', writedata.newUser)
+  .post('/newuser', urlencodedParser, writedata.newUser)
   .get('/', (req, res) => res.render('pages/index'))
 
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
--- models/writedata.js
+++ models/writedata.js
@@ -1,5 +1,3 @@
-var express = require('express');
-var bodyParser = require('body-parser');
 const crypt = require('bcrypt');
 const dbconnect = require('./../dbconnect.js');
 
