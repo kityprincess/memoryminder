3d873338e29c87ffe1198eb84ed14e74677c97b7
kityprincess <stowekm@byui.edu>
2018-07-14 17:09:38 -0700

    still trying to add to db1

--- dbconnect.js
+++ dbconnect.js
@@ -3,8 +3,4 @@ const { Pool } = require('pg-promise');
 const connectionString = process.env.DATABASE_URL;
 const pool = new Pool({connectionString: connectionString});
 
-const begin_transaction = 'begin';
-const rollback_transaction = 'rollback';
-const commit_transaction = 'commit';
-
 module.exports.pool = pool;
