038a7dffb636558260c978937e94d49408c5eca5
kityprincess <stowekm@byui.edu>
2018-07-18 17:18:41 -0700

    trying to get authentication to work3

--- models/getdata.js
+++ models/getdata.js
@@ -63,7 +63,7 @@ function getUser(req, res) {
 function getUserFromDb(username, callback) {
 	console.log('getUserFromDb called with user: ', username);
 
-	var sql = 'SELECT id, username, password, first_name, last_name FROM public.vip WHERE id = $1';
+	var sql = 'SELECT id, username, password, first_name, last_name FROM public.vip WHERE username = $1';
 
 	var params = [username];
 
